bb660e6ed72c73993f12d83c8465aef6a28d435f
WangJingCan <jxtory@sina.com>
2018-05-08 17:51:37 +0800

    201805081751

--- thinkphp/application/dm/controller/Datacenter.php
+++ thinkphp/application/dm/controller/Datacenter.php
@@ -6,6 +6,10 @@ class Datacenter extends Dmbase
 {
     public function index()
     {
+    	$mans_zaizhi = db("personnels")->where("status", "0")->count("id");
+    	$mans_lizhi = db("personnels")->where("status", "1")->count("id");
+    	$this->assign("mans_zaizhi", $mans_zaizhi);
+    	$this->assign("mans_lizhi", $mans_lizhi);
     	return $this->fetch();
     }
 
