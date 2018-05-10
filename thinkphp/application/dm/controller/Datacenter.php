e30aaa372f5a1d401ffac4a27a96ad42d1071513
Jahs <jxtory@sina.com>
2018-05-10 21:44:03 +0800

    201805102143
    修复·无数据时进入数据概览页面的错误

--- thinkphp/application/dm/controller/Datacenter.php
+++ thinkphp/application/dm/controller/Datacenter.php
@@ -12,25 +12,25 @@ class Datacenter extends Dmbase
     	$this->assign("mans_zaizhi", $mans_zaizhi);
     	$this->assign("mans_lizhi", $mans_lizhi);
 
-    	//部门统计
-    	$dept_level1 = db("dept1")->count("dept1");
-    	$this->assign("dept1", $dept_level1);
-    	$dept_level2 = db("dept2")->count("dept2");
-    	$this->assign("dept2", $dept_level2);
+        //部门统计
+        $dept_level1 = db("dept1")->count("dept1");
+        $this->assign("dept1", $dept_level1);
+        $dept_level2 = db("dept2")->count("dept2");
+        $this->assign("dept2", $dept_level2);
 
-    	//设备用量统计
-    	$deviceoutside = db("holders")->count("id");
-    	$this->assign("deviceoutside", $deviceoutside);
-    	$deviceinside = db("devices")
-    		->where("id", "not in", function($query){
-    			$query->table("dm_holders")->field("did");
-    		})
-    		->count("id");
+        //设备用量统计
+        $deviceoutside = db("holders")->count("id");
+        $this->assign("deviceoutside", $deviceoutside);
+        $deviceinside = db("devices")
+            ->where("id", "not in", function($query){
+                $query->table("dm_holders")->field("did");
+            })
+            ->count("id");
 
-    	$this->assign("deviceinside", $deviceinside);
+        $this->assign("deviceinside", $deviceinside);
 
-    	$devicesum = db("devices")->count("id");
-    	$this->assign("devicesum", $devicesum);
+        $devicesum = db("devices")->count("id");
+        $this->assign("devicesum", $devicesum);
 
         //设备特殊统计
         $devtypemax = db("devices")
@@ -44,7 +44,15 @@ class Datacenter extends Dmbase
 
         $this->assign("devtypemax", $devtypemax);
 
-    	return $this->fetch();
+        if(db("personnels")->count("id") == 0){
+            return $this->error("人员数据不完整");
+        }
+
+        if(db("devices")->count("id") == 0){
+            return $this->error("设备数据不完成");
+        }
+
+        return $this->fetch();
     }
 
 }
